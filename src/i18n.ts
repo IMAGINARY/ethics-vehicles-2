733721ac15658094bca22702fa480535e528b90e
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-04-16 14:34:05 -0700

    language switcher

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,17 @@
 import i18next from "i18next";
 
+const I18N_ATTR = "data-i18n";
+
 export function createI18nText(tag: string, key: string) {
   const el = document.createElement(tag);
   el.textContent = i18next.t(key);
-  el.setAttribute("data-i18n", key);
+  el.setAttribute(I18N_ATTR, key);
   return el;
+}
+
+export function refreshI18nText() {
+  for (const el of document.querySelectorAll(`[${I18N_ATTR}]`)) {
+    const key = el.getAttribute(I18N_ATTR) ?? "";
+    el.textContent = i18next.t(key);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { scenarios, Scenario } from "./scenarios";
 import en from "./locales/en.json";
 import de from "./locales/de.json";
 import Options from "./Options";
-import { createI18nText } from "./i18n";
+import { createI18nText, refreshI18nText } from "./i18n";
+
+const languages = [
+  { code: "en", name: "English" },
+  { code: "de", name: "Deutsch" },
+];
 
 await i18next.init({
   lng: "en", // TODO language detector
@@ -15,6 +20,7 @@ await i18next.init({
 let video: HTMLMediaElement;
 let menu: HTMLElement;
 let labelContainer: HTMLElement;
+let langSwitcher: HTMLButtonElement;
 
 function playIdle() {
   // Play idle video
@@ -94,12 +100,29 @@ function showScenario({ key, labels, videoSrc, options }: Scenario) {
   };
 }
 
+async function switchLanguage() {
+  const currentIndex = languages.findIndex(
+    ({ code }) => i18next.language === code
+  );
+  const { name, code } = languages[(currentIndex + 1) % languages.length];
+  await i18next.changeLanguage(code);
+  refreshI18nText();
+  langSwitcher.textContent = name;
+}
+
 window.onload = () => {
   video = document.getElementById("video") as HTMLMediaElement;
   menu = document.getElementById("menu")!;
   labelContainer = document.getElementById("labels")!;
+  langSwitcher = document.getElementById("lang-switcher") as HTMLButtonElement;
 
   document.getElementById("start-button")!.onclick = playIdle;
+  langSwitcher.onclick = switchLanguage;
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Tab") {
+      switchLanguage();
+    }
+  });
 };
 
 const idleVideo =

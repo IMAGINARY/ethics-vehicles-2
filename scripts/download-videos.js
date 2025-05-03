05cc275d527b75e479de33ec014684741d6d8132
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2025-05-02 19:17:53 -0700

    Download scenario videos.
    I forgot to add `scene_[n]_0.mp4` to the video list.

--- scripts/download-videos.js
+++ scripts/download-videos.js
@@ -9,12 +9,15 @@ console.log(`VIDEO_URL=${url}`);
 // TODO These should probably be configured somewhere
 const videos = [
   "idle.mp4",
+  "scene_1_0.mp4",
   "scene_1_1.mp4",
   "scene_1_2.mp4",
   "scene_1_3.mp4",
+  "scene_2_0.mp4",
   "scene_2_1.mp4",
   "scene_2_2.mp4",
   "scene_2_3.mp4",
+  "scene_3_0.mp4",
   "scene_3_1.mp4",
   "scene_3_2.mp4",
   "scene_3_3.mp4",
